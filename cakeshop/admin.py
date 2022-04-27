94c81ec9e7545cedd5f791038e9e796cac8c90a7
IlyaG96 <ilyag_96@icloud.com>
2022-04-27 21:33:11 +1100

    register models in admin.py

--- cakeshop/admin.py
+++ cakeshop/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from cakeshop.models import Order, Customer, Cake, Decoration, Topping, Shape, Advertisement, Berry, Height
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cake)
+class CakeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Height)
+class HeightAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Shape)
+class ShapeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Topping)
+class ToppingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Berry)
+class BerryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Decoration)
+class DecorationAdmin(admin.ModelAdmin):
+    pass
