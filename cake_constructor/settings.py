a1d2e8240860de6d128569c05adfbc3b1d88ec6d
Alexey Pesotsky <pesthedog@mail.ru>
2022-05-01 14:11:34 +0400

    Add common middleware

--- cake_constructor/settings.py
+++ cake_constructor/settings.py
@@ -35,6 +35,7 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
